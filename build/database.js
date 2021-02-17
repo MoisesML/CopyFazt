f91f5f054f3e2141bc05c2e815bf9fe42f7b5387
MoisesML <lazaromoises06@gmail.com>
2021-02-17 11:55:40 -0500

    CopyFazt Build

---
+++ build/database.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var _mongoose = _interopRequireDefault(require("mongoose"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+_mongoose["default"].connect("mongodb://localhost/companydb", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: true,
+  useCreateIndex: true
+}).then(function (db) {
+  return console.log('Db is connect');
+})["catch"](function (error) {
+  return console.log(error);
+});
